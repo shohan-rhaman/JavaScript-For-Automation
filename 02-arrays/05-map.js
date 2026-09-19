a3bf29c1edb375d2d18974473dd46fb3c7ae31fe
shohan-rhaman <shohanurrahman.contact@gmail.com>
2026-09-19 23:06:40 +0600

    map real business example

--- 02-arrays/05-map.js
+++ 02-arrays/05-map.js
@@ -1,9 +1,15 @@
 /*
 
-    Map() is buid in method of arrays\
+    Map() is buid in method of arrays
 
     Its main function:
     Creates a new array by operating on each item of an array.
+
+
+    3 core point about map():
+    - Operates on each item
+    - Creates a new array using the returned value
+    - Does not modify the original array
 
 */
 
@@ -26,8 +32,81 @@ const leads = [
     { name: "Mike", status: "new" }
 ];
 
-const userName = leads.map(lead =>{
+const leadNames = leads.map(lead =>{
     return lead.name
 })
 
-console.log(userName)
+console.log(leadNames)
+
+
+
+/*
+
+    Transform each lead into this structure using map():
+    name → fullName
+    email → contact
+
+*/
+
+
+const leadsData = [
+    {
+        name: "John",
+        email: "john@example.com"
+    },
+    {
+        name: "Sarah",
+        email: "sarah@example.com"
+    }
+];
+
+
+const output = leadsData.map(lead =>{
+    return {
+        fullName: lead.name,
+        contact: lead.email
+    }
+})
+
+console.log(output)
+
+
+
+/*
+
+Rule:
+- if status === "new" then isNew: true
+- if other status isNew: false
+
+*/
+
+
+const customerLeads = [
+    {
+        name: "John",
+        email: "john@example.com",
+        status: "new"
+    },
+    {
+        name: "Sarah",
+        email: "sarah@example.com",
+        status: "qualified"
+    },
+    {
+        name: "Mike",
+        email: "mike@example.com",
+        status: "new"
+    }
+];
+
+
+const customerLeadsData = customerLeads.map(lead =>{
+
+    return {
+        name: lead.name,
+        email: lead.email,
+        isNew: lead.status === "new"
+    }
+})
+
+console.log(customerLeadsData)
